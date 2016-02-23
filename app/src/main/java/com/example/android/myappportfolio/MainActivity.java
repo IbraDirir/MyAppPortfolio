a2dd540952e4c433a8b1bc723e6aff56db3e519a
IbraDirir <khaliildirir@gmail.com>
2016-02-23 16:13:07 +0300

    build the button projects styled the buttons and added toast messages to the buttons

--- app/src/main/java/com/example/android/myappportfolio/MainActivity.java
+++ app/src/main/java/com/example/android/myappportfolio/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.android.myappportfolio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,43 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+ public void SpotifyStreamer(View view){
+     Toast.makeText(this, "This button will launch the Spotify streamer project",Toast.
+             LENGTH_SHORT).show();
+
+
+ }
+    public void ScoresApp(View view){
+        Toast.makeText(this, "This button will launch the Scores  project",Toast.
+                LENGTH_SHORT).show();
+
+    }
+
+    public void libraryApp(View view){
+        Toast.makeText(this, "This button will launch the Library project",Toast.
+                LENGTH_SHORT).show();
+
+    }
+
+    public void buildItBigger(View view){
+        Toast.makeText(this, "This button will launch the Build it Bigger project",Toast.
+                LENGTH_SHORT).show();
+
+    }
+
+
+    public void xyzReader(View view){
+        Toast.makeText(this, "This button will launch the xyz reader  project",Toast.
+                LENGTH_SHORT).show();
+
+    }
+
+    public void myOwnApp(View view){
+        Toast.makeText(this, "This button will launch my capstone App",Toast.
+                LENGTH_SHORT).show();
+
+    }
+
 }
+
